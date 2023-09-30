0e817315428d83268137a1ff22085f8ac3f05276
heyra <zalomnova.eliz@gmail.com>
2023-10-01 00:31:02 +0300

    feat: most beautiful spline in the world typing for https://www.npmjs.com/package/cubic-spline

--- src/components/spline-canvas/SplineCanvas.vue
+++ src/components/spline-canvas/SplineCanvas.vue
@@ -11,6 +11,7 @@ import { Nullable } from 'src/models/generic';
 import { ref, Ref, watch } from 'vue';
 import { drawAxis } from 'components/spline-canvas/draw-axis';
 import { configStage } from 'components/spline-canvas/resources';
+import Spline from 'components/spline-canvas/draw-spline';
 
 const stageRef: Ref<Nullable<Konva.NodeConfig>> = ref(null);
 const axisLayerRef: Ref<Nullable<Konva.NodeConfig>> = ref(null);
@@ -29,7 +30,7 @@ const stageSetup = (
   spline: Konva.Line
 ) => {
   // Initialize an array to store control points
-  const controlPoints = [];
+  const controlPoints: Array<Konva.Circle> = [];
 
   // Function to add a draggable control point
   function addControlPoint(x: number, y: number) {
@@ -49,43 +50,40 @@ const stageSetup = (
     controlPoints.push(controlPoint);
   }
 
-  // Function to update the Catmull-Rom spline
   function updateSpline() {
-    if (controlPoints.length < 4) {
+    if (controlPoints.length < 5) {
       return;
     }
 
+    const adapter = new Spline(
+      controlPoints.map((p) => p.x()),
+      controlPoints.map((p) => p.y())
+    );
+
     const splinePoints = [];
 
-    for (let i = 1; i < controlPoints.length - 2; i++) {
-      const p0 = controlPoints[i - 1];
-      const p1 = controlPoints[i];
-      const p2 = controlPoints[i + 1];
-      const p3 = controlPoints[i + 2];
+    let leftmostX = controlPoints[0].x();
+    let rightmostX = controlPoints[0].x();
 
-      for (let t = 0; t <= 1; t += 0.01) {
-        const x =
-          0.5 *
-          (2 * p1.x() +
-            (-p0.x() + p2.x()) * t +
-            (2 * p0.x() - 5 * p1.x() + 4 * p2.x() - p3.x()) * t * t +
-            (-p0.x() + 3 * p1.x() - 3 * p2.x() + p3.x()) * t * t * t);
-
-        const y =
-          0.5 *
-          (2 * p1.y() +
-            (-p0.y() + p2.y()) * t +
-            (2 * p0.y() - 5 * p1.y() + 4 * p2.y() - p3.y()) * t * t +
-            (-p0.y() + 3 * p1.y() - 3 * p2.y() + p3.y()) * t * t * t);
-
-        splinePoints.push(x, y);
+    controlPoints.forEach((point) => {
+      const x = point.x();
+      if (x < leftmostX) {
+        leftmostX = x;
       }
-    }
+      if (x > rightmostX) {
+        rightmostX = x;
+      }
+    });
 
+    const delta = (rightmostX - leftmostX) / 100;
+    for (let i = 0; i < 100; i++) {
+      const x = leftmostX + i * delta;
+      splinePoints.push(x, adapter.at(x));
+    }
     spline.points(splinePoints);
   }
 
-  // Add initial control points
+  addControlPoint(10, 10);
   addControlPoint(50, 50);
   addControlPoint(100, 200);
   addControlPoint(300, 300);
--- src/components/spline-canvas/utils.ts
+++ src/components/spline-canvas/utils.ts
@@ -6,4 +6,55 @@ const makeYInverter = (stageConfig: typeof configStage) => (y: number) =>
 const bullshitPointsAdapter = (points: Array<{ x: number; y: number }>) =>
   points.flatMap(({ x, y }) => [x, y]);
 
-export { makeYInverter, bullshitPointsAdapter };
+const cubicSplineInterpolation = (points: Array<{ x: number; y: number }>) => {
+  const n = points.length - 1;
+  const h = [];
+  const alpha = [];
+  const l = [];
+  const mu: Array<number> = [];
+  const z: Array<number> = [];
+  const c: Array<number> = [];
+  const b: Array<number> = [];
+  const d: Array<number> = [];
+
+  // Step 1: Sort the points based on x-coordinates
+  points.sort((a, b) => a.x - b.x);
+
+  // Step 2: Calculate differences and initialize arrays
+  for (let i = 0; i < n; i++) {
+    h[i] = points[i + 1].x - points[i].x;
+    alpha[i] = (3 / h[i]) * (points[i + 1].y - points[i].y);
+  }
+
+  // Step 3: Calculate the tridiagonal system
+  for (let i = 1; i < n; i++) {
+    l[i] = 2 * (points[i + 1].x - points[i - 1].x) - h[i - 1] * mu[i - 1];
+    mu[i] = h[i] / l[i];
+    z[i] = (alpha[i] - h[i - 1] * z[i - 1]) / l[i];
+  }
+  console.log(l, mu, z);
+
+  // Step 4: Calculate the cubic spline coefficients
+  c[n] = 0;
+  for (let j = n - 1; j >= 0; j--) {
+    c[j] = z[j] - mu[j] * c[j + 1];
+    b[j] =
+      (points[j + 1].y - points[j].y) / h[j] -
+      (h[j] * (c[j + 1] + 2 * c[j])) / 3;
+    d[j] = (c[j + 1] - c[j]) / (3 * h[j]);
+  }
+
+  // Step 5: Define the spline function
+  function spline(x: number) {
+    let i = 0;
+    while (i < n && x > points[i + 1].x) {
+      i++;
+    }
+    const dx = x - points[i].x;
+    return points[i].y + b[i] * dx + c[i] * dx * dx + d[i] * dx * dx * dx;
+  }
+
+  return spline;
+};
+
+export { makeYInverter, bullshitPointsAdapter, cubicSplineInterpolation };
