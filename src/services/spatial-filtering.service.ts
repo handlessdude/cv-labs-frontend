52d089a1f9888427a0e2b102a4148c2d24137cb8
heyra <zalomnova.eliz@gmail.com>
2023-11-06 18:46:24 +0300

    fix: spatial filtering service resources naming

--- src/services/spatial-filtering.service.ts
+++ src/services/spatial-filtering.service.ts
@@ -2,12 +2,12 @@ import { HttpBasedService } from 'src/services/httb-based.service';
 import { GetImageParams, ImageSchema } from 'src/models/image-service';
 
 const BASE_PATH = '/spatial-filtering';
-const GEARS_INSPECTION_PATH = '/skeleton-enhancement';
+const SKELETON_ENHANCEMENT_PATH = '/skeleton-enhancement';
 
 class SpatialFilteringService extends HttpBasedService {
   async skeletonEnhancement(params: Partial<GetImageParams> = {}) {
     const res = await this.httpClient.get<Array<ImageSchema>>(
-      BASE_PATH + GEARS_INSPECTION_PATH,
+      BASE_PATH + SKELETON_ENHANCEMENT_PATH,
       {
         params,
       }
